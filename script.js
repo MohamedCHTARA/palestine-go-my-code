629c7ad20d8e79f48bf855e1031a263837f4126c
MohamedCHTARA <mohamedchtara5@gmail.com>
2024-07-26 11:36:02 +0100

    palestine go my code

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: "smooth"
+            });
+        });
+    });
+});
+
+        
+   
